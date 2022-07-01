8f3ae89b38f811250f2a23711476fa8aff87b5fe
Mo <mo@standardnotes.com>
2022-07-01 11:38:01 -0500

    chore: workflow

--- cdn/scripts/Compile.sh
+++ cdn/scripts/Compile.sh
@@ -2,11 +2,11 @@
 
 echo 'Building packages from' $(pwd)
 
-yarn clean && yarn build:packages
+yarn build:packages
 
 echo "Packaging packages assets and zips..."
 
-node cdn/scripts/Package.mjs
+node scripts/Package.mjs
 git add dist
 git add ../README.md
 (git commit -m 'chore(release): packages') || true
